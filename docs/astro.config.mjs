f2dd5fa59b6eee5cb0aa7aeae8d1eccc3dc246d6
Philipe Morais <philipe@MacBook-Pro-de-Philipe.local>
2024-11-12 15:23:49 -0300

    feat(docs) Alterate docs tech for Starlight and implement CI for auto deploy

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,26 +2,43 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import starlightImageZoomPlugin from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://mdsreq-fga-unb.github.io/2024.2-T03-Submin/',
+	base: '/docs',
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
+			plugins: [starlightImageZoomPlugin()],
+			title: 'Submin',
+			logo: {
+				replacesTitle: true,
+				dark: './src/assets/dark.svg',
+				light: './src/assets/light.svg',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/mdsreq-fga-unb/2024.2-T03-Submin',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Introdução',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Bem-vindo', slug: 'intro/introduction' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Visão de Produto e Projeto',
+					items: [
+						{ label: 'Cenário Atual do Cliente e do Negócio', slug: 'unidade-1/cenario'},
+						{ label: 'Soluções Propostas', slug: 'unidade-1/solucao'},
+						{ label: 'Estratégias de Engenharia de Software', slug: 'unidade-1/estrategias'},
+						{ label: 'Cronograma e Entregas', slug: 'unidade-1/cronograma'},
+						{ label: 'Interação Entre Equipe e Cliente', slug: 'unidade-1/interacao'},
+						{ label: 'Lições Aprendidas', slug: 'unidade-1/licoes'},
+						{ label: 'Referências Bibliográficas', slug: 'unidade-1/referencias'},
+					],
 				},
 			],
 			customCss: ['./src/tailwind.css'],
--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,6 +1,8 @@
 import colors from 'tailwindcss/colors';
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
+const accentColors = { 200: '#96D0FF	', 300: '#96D0FF', 600: '#6DACDF' };
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -8,7 +10,7 @@ export default {
 		extend: {
 			colors: {
 				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
+				accent: accentColors,
 				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
 				gray: colors.zinc,
 			},
